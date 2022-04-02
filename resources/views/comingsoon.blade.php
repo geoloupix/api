b628e7bad38d82b1e29dc399511f36edb2ec5eb6
MaitreRouge <gazmarcou@outlook.fr>
2022-04-02 21:02:36 +0200

    Built basic landing page

---
+++ resources/views/comingsoon.blade.php
@@ -0,0 +1,26 @@
+{{--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tailwindcss/ui@latest/dist/tailwind-ui.min.css">--}}
+<script src="https://cdn.tailwindcss.com"></script>
+<title>Geoloupix</title>
+
+<!-- component -->
+<div class="flex items-center justify-center h-screen bg-gray-200">
+    <div class="container">
+        <div class="bg-white rounded-lg shadow-lg p-5 md:p-20 mx-2">
+            <div class="text-center">
+                <h2 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+                    Geo<span class="text-amber-400">lou</span>pix
+                </h2>
+                <h3 class='text-xl md:text-3xl mt-10'>Coming Soon</h3>
+                <p class="text-md md:text-xl mt-10"><a class="hover:underline" href="https://geoloupix.fr/">Geoloupix</a> is an app to share your location or specific locations to your friends for free.</p>
+            </div>
+            <div class="flex flex-wrap mt-10 justify-center">
+                <div class="m-3">
+                    <a href="https://discord.gg/yRsZKbCjA8" title="Geoloupix On Discord"
+                       class="md:w-32 bg-white tracking-wide text-blue-600 font-bold rounded border-2 border-blue-600 hover:border-blue-600 hover:bg-blue-600 hover:text-white shadow-md py-2 px-6 inline-flex items-center">
+                        <span class="mx-auto">Discord</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
