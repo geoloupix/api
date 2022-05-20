9e1cf2849fe36ee023173f0e434264107aa8366b
Marc M <gazmarcou@outlook.fr>
2022-05-20 11:49:46 +0200

    Added some reading

--- app/Http/Controllers/API/LocationController.php
+++ app/Http/Controllers/API/LocationController.php
@@ -14,30 +14,45 @@ use Psy\Util\Json;
 class LocationController extends Controller
 {
 
+    /**
+     * Function used by the API to get all or a specific location(s)
+     * @param Request $request (location) id & (user) token checked by middleware
+     * @return JsonResponse
+     */
     public function get(Request $request): JsonResponse
     {
         //TODO: If location is shared to user, he needs to be able to use this endpoint to get information (actually sends a 403)
-        $location_id = $request['id']??null;
-        $user_id = (Token::find($request->header("X-Token")))->user_id;
+        $location_id = $request['id']??null; //Take the location id asked
+        $user_id = (Token::find($request->header("X-Token")))->user_id;//Find user_id
 
-        if ($location_id === null) $location = Location::all();
-        else $location = Location::find($location_id);
+        if ($location_id === null) $location = Location::all(); //If there is no location_id given then we send all of the users locations
+        else $location = Location::find($location_id); //Else we send the one asked
 
+        //If the used asked for a specific location BUT doesn't have the rights to access it we send a 403
         if($location_id !== null and $location->user_id != $user_id) return response()->json(["message" => "Insufficient permissions for location '${location_id}'"],403);
 
+        //Send the array, transform to json, and we're done for the day
         return response()->json($location);
     }
 
+    /**
+     * Function used by the API to create a new location
+     * @param Request $request checked by middleware
+     * @return JsonResponse
+     */
     public function store(Request $request): JsonResponse
     {
+        //Take the folder_id if present
         $folder_id = $request['folder_id'];
         $user_id = (Token::find($request->header("X-Token")))->user_id;
 
+        //If a folder was given we check if the user have the rights to the folder
         if (isset($folder_id)){
             $folder = Folder::find($folder_id);
             if($folder->user_id != $user_id) return response()->json(["message" => "Insufficient permissions for folder '${folder_id}'"],403);
         }
 
+        //create the location, insert it in the database
         $location = Location::create([
             "id" => Str::random(5),
             "name" => $request['name'],
@@ -52,7 +67,7 @@ class LocationController extends Controller
 
     public function delete(Request $request): JsonResponse
     {
-        //TODO: CHECK DATA LEAKS
+        //TODO: Check for flaws
         $id = $request['id'];
         $user_id = (Token::find($request->header("X-Token")))->user_id;
 
--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -16,12 +16,14 @@ class UserController extends Controller
 {
 
     /**
-     * @param Request $request
+     * Function used by the API to create a new user
+     * @param Request $request Verified by middleware
      * @return JsonResponse
      * @throws ValidationException
      */
     public function store(Request $request): JsonResponse
     {
+        //Creation of the user object and instantly insert it in the database
         $user = User::create([
             'uuid' => Str::uuid(),
             'username' => $request['username'],
@@ -29,8 +31,10 @@ class UserController extends Controller
             'password' => bcrypt($request['password'])
         ]);
 
+        //Now we create a new token for this user to be able to be "login"
         $user->createToken();
 
+        //Build a response, it's an array that is converted in json; http code is "201 created"
         return response()->json([
             "uuid" => $user->uuid,
             "username" => $user->username,
@@ -41,44 +45,64 @@ class UserController extends Controller
     }
 
     /**
-     * @param Request $request
+     * Funtion used by the API to log in an existing used
+     * @param Request $request Username NOT checked by middleware !
      * @return JsonResponse
      */
     public function login(Request $request): JsonResponse
     {
+        //Find the first user by the username given
         $user = User::where('username', $request['username'])->first();
+        //If not found we need to tell like the user exists to avoid security leak
         if(!isset($user)) return response()->json(["message" => "Wrong username or password"], 401);
+        //If wrong password, reject
         if(!$user->passwordCheck($request['password'])) return response()->json(["message" => "Wrong username or password"], 401);
 
+        //Create a token for the end user
         $user->createToken();
 
+        //Build the response as an array, transform it into json and send
         return response()->json([
             "uuid" => $user->uuid,
             "username" => $user->username,
             "email" => $user->email,
             "confirmed" => (bool)$user->confirmed,
             "token" => $user->getToken()->token
-        ], 202);
+        ], 200);
 
     }
 
+    /**
+     * Function used by the API to get an user from a request token given
+     * @param Request $request Token checked by middleware
+     * @return JsonResponse
+     */
     public function get(Request $request): JsonResponse
     {
+        //Get the token from the header, find it in the tokens table and take the user_id linked
         $user_id = (Token::find($request->header("X-Token")))->user_id;
+        //Then find the user in the users table which have the same user_id
         return response()->json(User::find($user_id));
     }
 
+    /**
+     * Function used by the API to patch an existing user (email/username)
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function patch(Request $request): JsonResponse
     {
-        $user_id = (Token::find($request->header("X-Token")))->user_id;
+        //REUSE the function to get an user from a token bc I'm lazy
         $user = $this->get($request);
 
+        //Set the new value if there is one given
         $user->email = $request['email']??$user->email; //TODO: Add reverification check after changing email adress.
         $user->username = $request['username']??$user->username;
         $user->save();
 
+        //Build the response as an array, transform it into json and send
         return response()->json([
-            "uuid" => $user_id,
+            "uuid" => $user->user_id,
             "username" => $user->username,
             "email" => $user->email,
             "confirmed" => (bool)$user->confirmed
